24f8fdcdf9c0eb11213b16d7b0dd0938fb64f1ac
unknown <solodovniki76@gmail.com>
2025-03-04 21:27:48 +0200

    First commit

---
+++ Kursova/Kursova.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <lcoal.h>
+
+
+using namespace std;
+
+int main() {
+    string usertype;
+    set local(LC_CTYPE, "rus");
+
+    while (true) {
+        cout << "Виберіть акаунт для входу:" << endl ;
+        cout << "1. Продавець"<< endl;
+        cout << "2. Покупець" << endl;
+        cout << "Ваш вибір: ";
+        cin >> usertype;
+
+        if (usertype == "1" || usertype == "продавець") {
+            cout << "Ви успішно увійшли як Продавець." << endl;
+            break;
+        }
+        else if (usertype == "2" || usertype == "покупець") {
+            cout << "Ви успішно увійшли як Покупець." << endl;
+            break;
+        }
+        else {
+            cout << "Невірний вибір, спробуйте ще раз.";
+        }
+    }
+
+    return 0;
+}
