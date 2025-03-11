1d3d3ade5d26df92bbcbf9c8ce548cffe1aa46ba
unknown <solodovniki76@gmail.com>
2025-03-11 22:13:23 +0200

    Add choice of categories for seller

--- Kursova/Kursova.cpp
+++ Kursova/Kursova.cpp
@@ -18,29 +18,56 @@ struct Product {
     vector<string> promocodes;
     vector<string> bonuses;
 
-int main() {
-    string usertype;
-    setlocale (LC_CTYPE, "rus");
+    int main() {
+        string usertype;
+        setlocale(LC_CTYPE, "rus");
 
-    while (true) {
-        cout << "Виберіть акаунт для входу:" << endl ;
-        cout << "1. Продавець"<< endl;
-        cout << "2. Покупець" << endl;
-        cout << "Ваш вибір: ";
-        cin >> usertype;
+        while (true) {
+            cout << "Виберіть акаунт для входу:" << endl;
+            cout << "1. Продавець" << endl;
+            cout << "2. Покупець" << endl;
+            cout << "Ваш вибір: ";
+            cin >> usertype;
 
-        if (usertype == "1" || usertype == "продавець") {
-            cout << "Ви успішно увійшли як Продавець." << endl;
-            break;
+            if (usertype == "1" || usertype == "продавець") {
+                cout << "Ви успішно увійшли як Продавець." << endl;
+
+                while (true) {
+                    int action;
+                    cout << endl;
+                    cout << "Що ви хочете зробити?" << endl;
+                    cout << "1. Додати товар" << endl;
+                    cout << "2. Відкрити/Закрити магазин" << endl;
+                    cout << "3. Створити акцію" << endl;
+                    cout << "4. Додати промокод" << endl;
+                    cout << "5. Додати бонус" << endl;
+                    cout << "6. Показати всі товари" << endl;
+                    cout << "7. Вийти" << endl;
+                    cout << "Ваш вибір: ";
+                    cin >> action;
+
+                    if (action == 1) {
+                        if (storeOpen == false) {
+                            cout << "Магазин зараз закритий. Відкрийте магазин спочатку." << endl;
+                            continue;
+                        }
+
+                        Product newProduct;
+                        int categoryChoice;
+                        break;
+                    }
+
+                    else if (usertype == "2" || usertype == "покупець") {
+                        cout << "Ви успішно увійшли як Покупець." << endl;
+                        break;
+                    }
+                    else {
+                        cout << "Невірний вибір, спробуйте ще раз.";
+                    }
+                }
+
+
+            }
         }
-        else if (usertype == "2" || usertype == "покупець") {
-            cout << "Ви успішно увійшли як Покупець." << endl;
-            break;
-        }
-        else {
-            cout << "Невірний вибір, спробуйте ще раз.";
-        }
+        return 0;
     }
-
-    return 0;
-}
