41c63818337b40f0cd78203c1f6e43dfe9271ade
unknown <solodovniki76@gmail.com>
2025-03-21 15:53:10 +0200

    Fix localistion

--- Kursova/Kursova.cpp
+++ Kursova/Kursova.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <vector>
-#include <locale.h>
+#include <locale>
 
 using namespace std;
 
@@ -17,62 +17,49 @@ struct Product {
 };
 
 int main() {
+    setlocale(LC_ALL, "uk_UA.UTF-8");
+
     string usertype;
-    setlocale(LC_CTYPE, "rus");
     vector<Product> store;
     bool storeOpen = true;
 
     while (true) {
-        cout << "Виберіть акаунт для входу:" << endl;
-        cout << "1. Продавець" << endl;
-        cout << "2. Покупець" << endl;
+        cout << "Виберіть акаунт для входу:\n";
+        cout << "1. Продавець\n";
+        cout << "2. Покупець\n";
         cout << "Ваш вибір: ";
         cin >> usertype;
 
         if (usertype == "1" || usertype == "продавець") {
-            cout << "Ви успішно увійшли як Продавець." << endl;
+            cout << "Ви успішно увійшли як Продавець.\n";
 
             while (true) {
                 int action;
-                cout << endl;
-                cout << "Що ви хочете зробити?" << endl;
-                cout << "1. Додати товар" << endl;
-                cout << "2. Відкрити/Закрити магазин" << endl;
-                cout << "3. Створити акцію" << endl;
-                cout << "4. Додати промокод" << endl;
-                cout << "5. Додати бонус" << endl;
-                cout << "6. Показати всі товари" << endl;
-                cout << "7. Вийти" << endl;
+                cout << "\nЩо ви хочете зробити?\n";
+                cout << "1. Додати товар\n";
+                cout << "2. Закрити магазин\n";
+                cout << "3. Створити акцію\n";
+                cout << "4. Додати промокод\n";
+                cout << "5. Додати бонус\n";
+                cout << "6. Показати всі товари\n";
+                cout << "7. Вийти\n";
                 cout << "Ваш вибір: ";
                 cin >> action;
+                cin.ignore();
 
                 if (action == 1) {
                     if (!storeOpen) {
-                        cout << "Магазин зараз закритий. Відкрийте магазин спочатку." << endl;
+                        cout << "Магазин закритий. Відкрийте магазин спочатку.\n";
                         continue;
                     }
 
                     Product newProduct;
-                    string categoryChoice;
+                    cout << "Оберіть категорію товару (іграшка, одежа, взуття, речі для дітей до 2 років): ";
+                    getline(cin, newProduct.category);
 
-                    cout << "Оберіть категорію товару: ";
-                    cin.ignore();
-                    getline(cin, categoryChoice);
-
-                    if (categoryChoice == "іграшка") {
-                        newProduct.category = "Іграшка";
-                    }
-                    else if (categoryChoice == "одежа") {
-                        newProduct.category = "Одежа";
-                    }
-                    else if (categoryChoice == "взуття") {
-                        newProduct.category = "Взуття";
-                    }
-                    else if (categoryChoice == "речі для дітей до 2 років") {
-                        newProduct.category = "Речі до 2 років";
-                    }
-                    else {
-                        cout << "Невірний вибір категорії." << endl;
+                    if (newProduct.category != "іграшка" && newProduct.category != "одежа" &&
+                        newProduct.category != "взуття" && newProduct.category != "речі для дітей до 2 років") {
+                        cout << "Невірний вибір категорії.\n";
                         continue;
                     }
 
@@ -82,92 +69,76 @@ int main() {
                     cin >> newProduct.price;
                     cout << "Введіть кількість товару: ";
                     cin >> newProduct.quantity;
+                    cin.ignore();
 
                     store.push_back(newProduct);
-                    cout << "Товар успішно доданий." << endl;
+                    cout << "Товар успішно доданий.\n";
                 }
                 else if (action == 2) {
-                    if (storeOpen) {
-                        storeOpen = false;
-                        cout << "Магазин зачинено." << endl;
-                    }
-                    else {
-                        storeOpen = true;
-                        cout << "Магазин відкрито." << endl;
-                    }
+                    storeOpen = false;
+                    cout << "Магазин закритий.\n";
                 }
                 else if (action == 3) {
                     string promo;
                     cout << "Введіть опис акції: ";
-                    cin.ignore();
                     getline(cin, promo);
                     for (auto& product : store) {
                         product.promotions.push_back(promo);
                     }
-                    cout << "Акція успішно додана." << endl;
+                    cout << "Акція додана.\n";
                 }
                 else if (action == 4) {
                     string code;
                     cout << "Введіть промокод: ";
-                    cin.ignore();
                     getline(cin, code);
                     for (auto& product : store) {
                         product.promocodes.push_back(code);
                     }
-                    cout << "Промокод успішно доданий." << endl;
+                    cout << "Промокод додано.\n";
                 }
                 else if (action == 5) {
                     string bonus;
                     cout << "Введіть опис бонусу: ";
-                    cin.ignore();
                     getline(cin, bonus);
                     for (auto& product : store) {
                         product.bonuses.push_back(bonus);
                     }
-                    cout << "Бонус успішно доданий." << endl;
+                    cout << "Бонус додано.\n";
                 }
                 else if (action == 6) {
-                    if (store.size() == 0) {
-                        cout << "Товарів поки немає." << endl;
+                    if (store.empty()) {
+                        cout << "Товарів поки немає.\n";
                     }
                     else {
-                        cout << "Список товарів:" << endl;
-                        for (int i = 0; i < store.size(); i++) {
+                        cout << "Список товарів:\n";
+                        for (size_t i = 0; i < store.size(); i++) {
                             cout << i + 1 << ". Назва: " << store[i].name << ", Категорія: " << store[i].category
-                                << ", Ціна: " << store[i].price << ", Кількість: " << store[i].quantity << endl;
+                                << ", Ціна: " << store[i].price << ", Кількість: " << store[i].quantity << "\n";
                             cout << "Акції: ";
-                            for (const auto& promo : store[i].promotions) {
-                                cout << promo << " ";
-                            }
-                            cout << endl;
-                            cout << "Промокоди: ";
-                            for (const auto& code : store[i].promocodes) {
-                                cout << code << " ";
-                            }
-                            cout << endl;
-                            cout << "Бонуси: ";
-                            for (const auto& bonus : store[i].bonuses) {
-                                cout << bonus << " ";
-                            }
-                            cout << endl;
+                            for (const auto& promo : store[i].promotions) cout << promo << " ";
+                            cout << "\nПромокоди: ";
+                            for (const auto& code : store[i].promocodes) cout << code << " ";
+                            cout << "\nБонуси: ";
+                            for (const auto& bonus : store[i].bonuses) cout << bonus << " ";
+                            cout << "\n";
                         }
                     }
                 }
                 else if (action == 7) {
-                    cout << "Вихід з акаунту Продавець." << endl;
+                    cout << "Вихід з акаунту Продавець.\n";
                     break;
                 }
                 else {
-                    cout << "Невірний вибір, спробуйте ще раз." << endl;
+                    cout << "Невірний вибір, спробуйте ще раз.\n";
                 }
             }
         }
         else if (usertype == "2" || usertype == "покупець") {
-            cout << "Ви успішно увійшли як Покупець." << endl;
+            cout << "Ви успішно увійшли як Покупець.\n";
             break;
         }
         else {
-            cout << "Невірний вибір, спробуйте ще раз." << endl;
+            cout << "Невірний вибір, спробуйте ще раз.\n";
         }
     }
 
